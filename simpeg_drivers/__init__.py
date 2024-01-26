3cb4e87bc3156dad41e4941d1d467e81b5e60368
benjamink <benjamink@mirageoscience.com>
2024-01-26 13:40:19 -0800

    update path to drivers in DRIVER_MAP

--- simpeg_drivers/__init__.py
+++ simpeg_drivers/__init__.py
@@ -28,57 +28,57 @@ def assets_path() -> Path:
 
 DRIVER_MAP = {
     "direct current 3d": (
-        "geoapps.inversion.electricals.direct_current.three_dimensions.driver",
+        "simpeg_drivers.electricals.direct_current.three_dimensions.driver",
         "DirectCurrent3DDriver",
     ),
     "direct current 2d": (
-        "geoapps.inversion.electricals.direct_current.two_dimensions.driver",
+        "simpeg_drivers.electricals.direct_current.two_dimensions.driver",
         "DirectCurrent2DDriver",
     ),
     "direct current pseudo 3d": (
-        "geoapps.inversion.electricals.direct_current.pseudo_three_dimensions.driver",
+        "simpeg_drivers.electricals.direct_current.pseudo_three_dimensions.driver",
         "DirectCurrentPseudo3DDriver",
     ),
     "induced polarization 3d": (
-        "geoapps.inversion.electricals.induced_polarization.three_dimensions.driver",
+        "simpeg_drivers.electricals.induced_polarization.three_dimensions.driver",
         "InducedPolarization3DDriver",
     ),
     "induced polarization 2d": (
-        "geoapps.inversion.electricals.induced_polarization.two_dimensions.driver",
+        "simpeg_drivers.electricals.induced_polarization.two_dimensions.driver",
         "InducedPolarization2DDriver",
     ),
     "induced polarization pseudo 3d": (
-        "geoapps.inversion.electricals.induced_polarization.pseudo_three_dimensions.driver",
+        "simpeg_drivers.electricals.induced_polarization.pseudo_three_dimensions.driver",
         "InducedPolarizationPseudo3DDriver",
     ),
     "joint surveys": (
-        "geoapps.inversion.joint.joint_surveys.driver",
+        "simpeg_drivers.joint.joint_surveys.driver",
         "JointSurveyDriver",
     ),
     "fem": (
-        "geoapps.inversion.airborne_electromagnetics.frequency_domain.driver",
+        "simpeg_drivers.airborne_electromagnetics.frequency_domain.driver",
         "FrequencyDomainElectromagneticsDriver",
     ),
     "joint cross gradient": (
-        "geoapps.inversion.joint.joint_cross_gradient.driver",
+        "simpeg_drivers.joint.joint_cross_gradient.driver",
         "JointCrossGradientDriver",
     ),
     "tdem": (
-        "geoapps.inversion.electromagnetics.time_domain.driver",
+        "simpeg_drivers.electromagnetics.time_domain.driver",
         "TimeDomainElectromagneticsDriver",
     ),
     "magnetotellurics": (
-        "geoapps.inversion.natural_sources.magnetotellurics.driver",
+        "simpeg_drivers.natural_sources.magnetotellurics.driver",
         "MagnetotelluricsDriver",
     ),
-    "tipper": ("geoapps.inversion.natural_sources.tipper.driver", "TipperDriver"),
-    "gravity": ("geoapps.inversion.potential_fields.gravity.driver", "GravityDriver"),
+    "tipper": ("simpeg_drivers.natural_sources.tipper.driver", "TipperDriver"),
+    "gravity": ("simpeg_drivers.potential_fields.gravity.driver", "GravityDriver"),
     "magnetic scalar": (
-        "geoapps.inversion.potential_fields.magnetic_scalar.driver",
+        "simpeg_drivers.potential_fields.magnetic_scalar.driver",
         "MagneticScalarDriver",
     ),
     "magnetic vector": (
-        "geoapps.inversion.potential_fields.magnetic_vector.driver",
+        "simpeg_drivers.potential_fields.magnetic_vector.driver",
         "MagneticVectorDriver",
     ),
 }
