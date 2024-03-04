6b6a7c30254cab2cc6ee2770aa66838a6f8b999a
dominiquef <dominiquef@mirageoscience.com>
2024-03-04 14:53:25 -0800

    Remove abstract factory in favor of entity_factory

--- simpeg_drivers/components/factories/entity_factory.py
+++ simpeg_drivers/components/factories/entity_factory.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2022-2023 Mira Geoscience Ltd.
+#  Copyright (c) 2023-2024 Mira Geoscience Ltd.
 #
 #  This file is part of simpeg_drivers package.
 #
@@ -10,19 +10,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
+from geoh5py.objects import Grid2D, Points, PotentialElectrode
+
 if TYPE_CHECKING:
     from simpeg_drivers.components.data import InversionData
 
-import numpy as np
-from geoh5py.objects import CurrentElectrode, Curve, Grid2D, Points, PotentialElectrode
 
-from simpeg_drivers.components.factories.abstract_factory import AbstractFactory
-
-
-class EntityFactory(AbstractFactory):
+class EntityFactory:
     def __init__(self, params):
         self.params = params
-        super().__init__(params)
 
     @property
     def factory_type(self):
@@ -32,23 +29,13 @@ class EntityFactory(AbstractFactory):
     @property
     def concrete_object(self):
         """Returns a geoh5py object to be constructed by the build method."""
-        if "current" in self.factory_type or "polarization" in self.factory_type:
-            return PotentialElectrode, CurrentElectrode
-
-        elif isinstance(self.params.data_object, Grid2D):
+        if isinstance(self.params.data_object, Grid2D):
             return Points
 
-        else:
-            return type(self.params.data_object)
+        return type(self.params.data_object)
 
     def build(self, inversion_data: InversionData):
         """Constructs geoh5py object for provided inversion type."""
-
-        entity = self._build(inversion_data)
-
-        return entity
-
-    def _build(self, inversion_data: InversionData):
         if isinstance(self.params.data_object, Grid2D):
             entity = inversion_data.create_entity(
                 "Data", inversion_data.locations, geoh5_object=self.concrete_object
@@ -60,8 +47,11 @@ class EntityFactory(AbstractFactory):
                 "copy_children": False,
             }
 
-            if np.any(~inversion_data.mask):
-                if isinstance(self.params.data_object, PotentialElectrode):
+            if inversion_data.mask is not None and np.any(~inversion_data.mask):
+                if (
+                    isinstance(self.params.data_object, PotentialElectrode)
+                    and self.params.data_object.cells is not None
+                ):
                     active_poles = np.zeros(
                         self.params.data_object.n_vertices, dtype=bool
                     )
@@ -91,11 +81,3 @@ class EntityFactory(AbstractFactory):
             )
 
         return entity
-
-    @staticmethod
-    def _prune_from_indices(curve: Curve, cell_indices: np.ndarray):
-        cells = curve.cells[cell_indices]
-        uni_ids, ids = np.unique(cells, return_inverse=True)
-        locations = curve.vertices[uni_ids, :]
-        cells = np.arange(uni_ids.shape[0], dtype="uint32")[ids].reshape((-1, 2))
-        return locations, cells
