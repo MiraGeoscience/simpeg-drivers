3c8904fbd749437a03245f5a9ee5f16f01b66279
domfournier <dominiquef@mirageoscience.com>
2024-04-22 10:04:29 -0700

    Update docs/inversion/index.py
    Co-authored-by: benk-mira <81254271+benk-mira@users.noreply.github.com>

--- docs/inversion/index.py
+++ docs/inversion/index.py
@@ -14,7 +14,7 @@
 
 # # Geophysical Inversions
 #
-# This module documents the use of [SimPEG](simpeg.xyz) for geophysical data inversion with user-interface (UIjson) made available through the [Mira Geoscience-geoapps](https://geoapps.readthedocs.io/en/latest/) project. While the code itself has its own documentation, there is a need to demonstrate the effect of parameters controlling the inversion. This document is meant to be a reference guide with practical examples to help practitioners with their inversion work.
+# This module documents the use of [SimPEG](simpeg.xyz) for geophysical data inversion within Geoscience ANALYST through the `UIJson <https://mirageoscience-geoh5py.readthedocs-hosted.com/en/latest/content/uijson_format/index.html>`_ rendered user-interface.  This is accomplished by executing driver scripts within the `simpeg-drivers <https://github.com/MiraGeoscience/simpeg-drivers>`_ package. This document is meant to be a reference guide with practical examples demonstrating parameters exposed by the interface and their effect on a given inversion result.
 #
 #
 # - [Inversion Fundamentals](fundamentals.ipynb): An overview of the inversion framework.
