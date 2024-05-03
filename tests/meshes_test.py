3f64408c87352b3c95b1b7df14c9feffb2772937
benjamink <benjamink@mirageoscience.com>
2024-05-03 10:35:55 -0700

    Add test to check inversion (seems to work)

--- tests/meshes_test.py
+++ tests/meshes_test.py
@@ -36,8 +36,8 @@ def setup_params(tmp_path):
         background=0.0,
         anomaly=0.05,
         refinement=(2,),
-        n_electrodes=2,
-        n_lines=2,
+        n_electrodes=4,
+        n_lines=4,
         inversion_type="magnetic_vector",
     )
 
@@ -112,15 +112,85 @@ def test_ensure_cell_convention(tmp_path):
         data = ws.get_entity("Iteration_0_tmi")
         test_data = data[0].values
         validation_data = data[1].values
+        active = ws.get_entity("active_cells")
+        test_active = active[0].values
+        validation_active = active[1].values
 
         test_mesh = InversionMesh.ensure_cell_convention(test_mesh)
         assert test_mesh.w_cell_size == 5.0
         assert test_mesh.origin["z"] == validation_mesh.origin["z"]
         assert np.allclose(test_mesh.centroids, validation_mesh.centroids)
         assert np.allclose(test_data, validation_data)
+        assert np.allclose(test_active, validation_active)
 
-        test_mesh.rotation = 20.0
+
+def test_raise_on_rotated_negative_cell_size(tmp_path):
+    ws, params = setup_params(tmp_path)
+    mesh = params.mesh
+    mesh.rotation = 20.0
+    mesh.w_cell_size *= -1
+    msg = "Cannot convert negative cell sizes for rotated mesh."
+    with pytest.raises(ValueError, match=msg):
+        InversionMesh.ensure_cell_convention(mesh)
+    ws.close()
+
+
+def test_negative_cell_size_inversion(tmp_path):
+
+    ws, params = setup_params(tmp_path)
+
+    validation_mesh = params.mesh.copy(copy_children=True)
+    validation_mesh.name = "validation"
+    inducing_field = (50000.0, 90.0, 0.0)
+    validation_params = MagneticVectorParams(
+        geoh5=ws,
+        mesh=validation_mesh,
+        starting_model=validation_mesh.get_data("model")[0],
+        inducing_field_strength=inducing_field[0],
+        inducing_field_inclination=inducing_field[1],
+        inducing_field_declination=inducing_field[2],
+        data_object=params.data_object.copy(copy_children=False),
+        topography_object=params.topography_object.copy(),
+        tmi_channel_bool=True,
+        forward_only=True,
+        out_group=SimPEGGroup.create(ws, name="validation_group"),
+    )
+    driver = MagneticVectorDriver(validation_params)
+    driver.run()
+    ws.close()
+
+    with ws.open():
+        data_object = ws.get_entity("survey")[0]
+        data = ws.get_entity("Iteration_0_tmi")[0].copy()
+        data.name = "tmi_data"
+
+        test_mesh = params.mesh.copy(copy_children=True)
+        test_mesh.name = "test"
+        test_mesh.origin["z"] += test_mesh.w_count * test_mesh.w_cell_size
         test_mesh.w_cell_size *= -1
-        msg = "Cannot convert negative cell sizes for rotated mesh."
-        with pytest.raises(ValueError, match=msg):
-            InversionMesh.ensure_cell_convention(test_mesh)
+
+        test_params = MagneticVectorParams(
+            geoh5=ws,
+            mesh=test_mesh,
+            starting_model=1e-4,
+            reference_model=0.0,
+            inducing_field_strength=inducing_field[0],
+            inducing_field_inclination=inducing_field[1],
+            inducing_field_declination=inducing_field[2],
+            data_object=data_object,
+            tmi_channel=data,
+            tmi_uncertainty=4.0,
+            topography_object=params.topography_object.copy(),
+            out_group=SimPEGGroup.create(ws, name="test_group"),
+            s_norm=0.0,
+            x_norm=1.0,
+            y_norm=1.0,
+            z_norm=1.0,
+            gradient_type="components",
+            initial_beta_ratio=1e1,
+            prctile=100,
+            store_sensitivities="ram",
+            max_global_iterations=2,
+        )
+        driver = MagneticVectorDriver(test_params)
+        driver.run()
