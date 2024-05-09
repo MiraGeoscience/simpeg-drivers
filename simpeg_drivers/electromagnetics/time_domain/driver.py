93cb4c99dcdecacd9e2add3fd6345af16e99cd50
dominiquef <dominiquef@mirageoscience.com>
2024-05-09 10:29:29 -0700

    Clean ups

--- simpeg_drivers/electromagnetics/time_domain/driver.py
+++ simpeg_drivers/electromagnetics/time_domain/driver.py
@@ -37,7 +37,7 @@ class TimeDomainElectromagneticsDriver(InversionDriver):
     def __init__(self, params: TimeDomainElectromagneticsParams):
         super().__init__(params)
 
-    def get_tiles(self):
+    def get_tiles(self) -> list[np.ndarray]:
         """
         Special method to tile the data based on the transmitters center locations.
 
@@ -45,55 +45,46 @@ class TimeDomainElectromagneticsDriver(InversionDriver):
         Second, if the number of groups is less than the number of 'tile_spatial' value, the groups are
         further divided into groups based on the clustering of receiver locations.
         """
-        if isinstance(self.params.data_object, LargeLoopGroundTEMReceivers):
+        if not isinstance(self.params.data_object, LargeLoopGroundTEMReceivers):
+            return super().get_tiles()
 
-            tx_ids = self.params.data_object.transmitters.tx_id_property.values
-
-            uids = np.unique(tx_ids)
-
-            n_groups = np.min([len(uids), self.params.tile_spatial])
-            locations = []
-            for uid in uids:
-                locations.append(
-                    np.mean(
-                        self.params.data_object.transmitters.vertices[tx_ids == uid],
-                        axis=0,
-                    )
+        tx_ids = self.params.data_object.transmitters.tx_id_property.values
+        unique_tile_ids = np.unique(tx_ids)
+        n_groups = np.min([len(unique_tile_ids), self.params.tile_spatial])
+        locations = []
+        for uid in unique_tile_ids:
+            locations.append(
+                np.mean(
+                    self.params.data_object.transmitters.vertices[tx_ids == uid],
+                    axis=0,
                 )
+            )
 
-            tx_tiles = tile_locations(
-                np.vstack(locations),
-                n_groups,
+        # Tile transmitters spatially by loop center
+        tx_tiles = tile_locations(
+            np.vstack(locations),
+            n_groups,
+            method="kmeans",
+        )
+        receivers_tx_ids = self.params.data_object.tx_id_property.values
+        tiles = []
+        for t_id, group in enumerate(tx_tiles):
+            sub_group = []
+            for value in group:
+                receiver_ind = receivers_tx_ids == unique_tile_ids[value]
+                sub_group.append(np.where(receiver_ind)[0])
+
+            tiles.append(np.hstack(sub_group))
+
+        # If number of tiles remaining, brake up receivers spatially per transmitter
+        while len(tiles) < self.params.tile_spatial:
+            largest_group = np.argmax([len(tile) for tile in tiles])
+            tile = tiles.pop(largest_group)
+            new_tiles = tile_locations(
+                self.params.data_object.vertices[tile],
+                2,
                 method="kmeans",
             )
-            rx_ids = self.params.data_object.tx_id_property.values
-            tiles = []
-            counter = {}
-            for t_id, group in enumerate(tx_tiles):
-                sub_group = []
-                for value in group:
-                    receiver_ind = rx_ids == uids[value]
-                    sub_group.append(np.where(receiver_ind)[0])
-                    counter[t_id] = counter.get(t_id, 0) + np.sum(receiver_ind)
-
-                tiles.append(np.hstack(sub_group))
-
-            while len(tiles) < self.params.tile_spatial:
-                largest_group = np.argmax([len(tile) for tile in tiles])
-                tile = tiles.pop(largest_group)
-                new_tiles = tile_locations(
-                    self.params.data_object.vertices[tile],
-                    2,
-                    method="kmeans",
-                )
-                tiles += [tile[new_tiles[0]], tile[new_tiles[1]]]
-
-        else:
-            locations = self.inversion_data.locations
-            tiles = tile_locations(
-                locations,
-                self.params.tile_spatial,
-                method="kmeans",
-            )
+            tiles += [tile[new_tiles[0]], tile[new_tiles[1]]]
 
         return tiles
