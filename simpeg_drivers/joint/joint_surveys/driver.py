d7abcb1f17142a9049ff82754caaf8606ae4bf5a
dominiquef <dominiquef@mirageoscience.com>
2024-05-29 14:59:29 -0700

    Fix tile id on save directives
    (cherry picked from commit 21f33a4c711a55774b9d7563c043a8a8d0f2295c)

--- simpeg_drivers/joint/joint_surveys/driver.py
+++ simpeg_drivers/joint/joint_surveys/driver.py
@@ -84,29 +84,28 @@ class JointSurveyDriver(BaseJointDriver):
                 count = 0
                 for driver in self.drivers:
                     driver_directives = DirectivesFactory(driver)
-                    save_data = driver_directives.save_iteration_data_directive
-
-                    n_tiles = len(driver.data_misfit.objfcts)
-                    save_data.joint_index = [count + ii for ii in range(n_tiles)]
-                    count += n_tiles
 
                     save_model = driver_directives.save_iteration_model_directive
                     save_model.transforms = [
                         driver.data_misfit.model_map
                     ] + save_model.transforms
-                    directives_list += [
-                        save_data,
-                        save_model,
-                    ]
+                    directives_list.append(save_model)
 
-                    for directive in [
+                    n_tiles = len(driver.data_misfit.objfcts)
+                    for name in [
+                        "save_iteration_data_directive",
+                        "save_iteration_residual_directive",
                         "save_iteration_apparent_resistivity_directive",
                         "vector_inversion_directive",
                     ]:
-                        if getattr(driver_directives, directive) is not None:
-                            directives_list.append(
-                                getattr(driver_directives, directive)
-                            )
+                        directive = getattr(driver_directives, name)
+                        if directive is not None:
+                            directive.joint_index = [
+                                count + ii for ii in range(n_tiles)
+                            ]
+                            directives_list.append(directive)
+
+                    count += n_tiles
 
                 self._directives = DirectivesFactory(self)
                 global_model_save = self._directives.save_iteration_model_directive
