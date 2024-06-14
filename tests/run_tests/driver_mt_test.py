b9927c35001334759bd50a1ae9fcbb412137bdcc
dominiquef <dominiquef@mirageoscience.com>
2024-06-13 21:37:39 -0700

    Only mt inv again

--- tests/run_tests/driver_mt_test.py
+++ tests/run_tests/driver_mt_test.py
@@ -81,121 +81,121 @@ def test_magnetotellurics_fwr_run(
 
 
 def test_magnetotellurics_run(tmp_path: Path, max_iterations=1, pytest=True):
-    pass
-    # workpath = tmp_path / "inversion_test.ui.geoh5"
-    # if pytest:
-    #     workpath = (
-    #         tmp_path.parent
-    #         / "test_magnetotellurics_fwr_run0"
-    #         / "inversion_test.ui.geoh5"
-    #     )
-    #
-    # with Workspace(workpath) as geoh5:
-    #     survey = geoh5.get_entity("survey")[0].copy(copy_children=False)
-    #     mesh = geoh5.get_entity("mesh")[0]
-    #     topography = geoh5.get_entity("topography")[0]
-    #
-    #     data = {}
-    #     uncertainties = {}
-    #     components = {
-    #         "zxx_real": "Zxx (real)",
-    #         "zxx_imag": "Zxx (imag)",
-    #         "zxy_real": "Zxy (real)",
-    #         "zxy_imag": "Zxy (imag)",
-    #         "zyx_real": "Zyx (real)",
-    #         "zyx_imag": "Zyx (imag)",
-    #         "zyy_real": "Zyy (real)",
-    #         "zyy_imag": "Zyy (imag)",
-    #     }
-    #
-    #     for comp, cname in components.items():
-    #         data[cname] = []
-    #         # uncertainties[f"{cname} uncertainties"] = {}
-    #         uncertainties[f"{cname} uncertainties"] = []
-    #         for ind in range(len(survey.channels)):
-    #             data_entity = geoh5.get_entity(f"Iteration_0_{comp}_[{ind}]")[0].copy(
-    #                 parent=survey
-    #             )
-    #             data[cname].append(data_entity)
-    #
-    #             uncert = survey.add_data(
-    #                 {
-    #                     f"uncertainty_{comp}_[{ind}]": {
-    #                         "values": np.ones_like(data_entity.values)
-    #                         * np.percentile(np.abs(data_entity.values), 10)
-    #                     }
-    #                 }
-    #             )
-    #             uncertainties[f"{cname} uncertainties"].append(
-    #                 uncert.copy(parent=survey)
-    #             )
-    #
-    #     data_groups = survey.add_components_data(data)
-    #     uncert_groups = survey.add_components_data(uncertainties)
-    #
-    #     data_kwargs = {}
-    #     for comp, data_group, uncert_group in zip(
-    #         components, data_groups, uncert_groups
-    #     ):
-    #         data_kwargs[f"{comp}_channel"] = data_group.uid
-    #         data_kwargs[f"{comp}_uncertainty"] = uncert_group.uid
-    #
-    #     orig_zyy_real_1 = geoh5.get_entity("Iteration_0_zyy_real_[0]")[0].values
-    #
-    #     # Run the inverse
-    #     params = MagnetotelluricsParams(
-    #         geoh5=geoh5,
-    #         mesh=mesh.uid,
-    #         topography_object=topography.uid,
-    #         resolution=0.0,
-    #         data_object=survey.uid,
-    #         starting_model=0.01,
-    #         reference_model=0.01,
-    #         alpha_s=1.0,
-    #         s_norm=1.0,
-    #         x_norm=1.0,
-    #         y_norm=1.0,
-    #         z_norm=1.0,
-    #         gradient_type="components",
-    #         z_from_topo=False,
-    #         upper_bound=0.75,
-    #         conductivity_model=1e-2,
-    #         max_global_iterations=max_iterations,
-    #         initial_beta_ratio=1e2,
-    #         prctile=100,
-    #         store_sensitivities="ram",
-    #         **data_kwargs,
-    #     )
-    #     params.write_input_file(path=tmp_path, name="Inv_run")
-    #     driver = MagnetotelluricsDriver.start(str(tmp_path / "Inv_run.ui.json"))
-    #
-    # with geoh5.open() as run_ws:
-    #     output = get_inversion_output(
-    #         driver.params.geoh5.h5file, driver.params.out_group.uid
-    #     )
-    #     output["data"] = orig_zyy_real_1
-    #     if pytest:
-    #         check_target(output, target_run, tolerance=0.5)
-    #         nan_ind = np.isnan(run_ws.get_entity("Iteration_0_model")[0].values)
-    #         inactive_ind = run_ws.get_entity("active_cells")[0].values == 0
-    #         assert np.all(nan_ind == inactive_ind)
-    #
-    # # test that one channel works
-    # data_kwargs = {k: v for k, v in data_kwargs.items() if "zxx_real" in k}
-    # geoh5.open()
-    # params = MagnetotelluricsParams(
-    #     geoh5=geoh5,
-    #     mesh=geoh5.get_entity("mesh")[0].uid,
-    #     topography_object=topography.uid,
-    #     data_object=survey.uid,
-    #     starting_model=0.01,
-    #     conductivity_model=1e-2,
-    #     max_global_iterations=0,
-    #     # store_sensitivities="ram",
-    #     **data_kwargs,
-    # )
-    # params.write_input_file(path=tmp_path, name="Inv_run")
-    # MagnetotelluricsDriver.start(str(tmp_path / "Inv_run.ui.json"))
+    # pass
+    workpath = tmp_path / "inversion_test.ui.geoh5"
+    if pytest:
+        workpath = (
+            tmp_path.parent
+            / "test_magnetotellurics_fwr_run0"
+            / "inversion_test.ui.geoh5"
+        )
+
+    with Workspace(workpath) as geoh5:
+        survey = geoh5.get_entity("survey")[0].copy(copy_children=False)
+        mesh = geoh5.get_entity("mesh")[0]
+        topography = geoh5.get_entity("topography")[0]
+
+        data = {}
+        uncertainties = {}
+        components = {
+            "zxx_real": "Zxx (real)",
+            "zxx_imag": "Zxx (imag)",
+            "zxy_real": "Zxy (real)",
+            "zxy_imag": "Zxy (imag)",
+            "zyx_real": "Zyx (real)",
+            "zyx_imag": "Zyx (imag)",
+            "zyy_real": "Zyy (real)",
+            "zyy_imag": "Zyy (imag)",
+        }
+
+        for comp, cname in components.items():
+            data[cname] = []
+            # uncertainties[f"{cname} uncertainties"] = {}
+            uncertainties[f"{cname} uncertainties"] = []
+            for ind in range(len(survey.channels)):
+                data_entity = geoh5.get_entity(f"Iteration_0_{comp}_[{ind}]")[0].copy(
+                    parent=survey
+                )
+                data[cname].append(data_entity)
+
+                uncert = survey.add_data(
+                    {
+                        f"uncertainty_{comp}_[{ind}]": {
+                            "values": np.ones_like(data_entity.values)
+                            * np.percentile(np.abs(data_entity.values), 10)
+                        }
+                    }
+                )
+                uncertainties[f"{cname} uncertainties"].append(
+                    uncert.copy(parent=survey)
+                )
+
+        data_groups = survey.add_components_data(data)
+        uncert_groups = survey.add_components_data(uncertainties)
+
+        data_kwargs = {}
+        for comp, data_group, uncert_group in zip(
+            components, data_groups, uncert_groups
+        ):
+            data_kwargs[f"{comp}_channel"] = data_group.uid
+            data_kwargs[f"{comp}_uncertainty"] = uncert_group.uid
+
+        orig_zyy_real_1 = geoh5.get_entity("Iteration_0_zyy_real_[0]")[0].values
+
+        # Run the inverse
+        params = MagnetotelluricsParams(
+            geoh5=geoh5,
+            mesh=mesh.uid,
+            topography_object=topography.uid,
+            resolution=0.0,
+            data_object=survey.uid,
+            starting_model=0.01,
+            reference_model=0.01,
+            alpha_s=1.0,
+            s_norm=1.0,
+            x_norm=1.0,
+            y_norm=1.0,
+            z_norm=1.0,
+            gradient_type="components",
+            z_from_topo=False,
+            upper_bound=0.75,
+            conductivity_model=1e-2,
+            max_global_iterations=max_iterations,
+            initial_beta_ratio=1e2,
+            prctile=100,
+            store_sensitivities="ram",
+            **data_kwargs,
+        )
+        params.write_input_file(path=tmp_path, name="Inv_run")
+        driver = MagnetotelluricsDriver.start(str(tmp_path / "Inv_run.ui.json"))
+
+    with geoh5.open() as run_ws:
+        output = get_inversion_output(
+            driver.params.geoh5.h5file, driver.params.out_group.uid
+        )
+        output["data"] = orig_zyy_real_1
+        if pytest:
+            check_target(output, target_run, tolerance=0.5)
+            nan_ind = np.isnan(run_ws.get_entity("Iteration_0_model")[0].values)
+            inactive_ind = run_ws.get_entity("active_cells")[0].values == 0
+            assert np.all(nan_ind == inactive_ind)
+
+    # test that one channel works
+    data_kwargs = {k: v for k, v in data_kwargs.items() if "zxx_real" in k}
+    geoh5.open()
+    params = MagnetotelluricsParams(
+        geoh5=geoh5,
+        mesh=geoh5.get_entity("mesh")[0].uid,
+        topography_object=topography.uid,
+        data_object=survey.uid,
+        starting_model=0.01,
+        conductivity_model=1e-2,
+        max_global_iterations=0,
+        # store_sensitivities="ram",
+        **data_kwargs,
+    )
+    params.write_input_file(path=tmp_path, name="Inv_run")
+    MagnetotelluricsDriver.start(str(tmp_path / "Inv_run.ui.json"))
 
 
 if __name__ == "__main__":
