4e6a21a7ec19648120ca26f418dc1dddf9ad0c43
dominiquef <dominiquef@mirageoscience.com>
2024-07-17 11:38:44 -0700

    Add content for tipper data

--- docs/case_study/data.py
+++ docs/case_study/data.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.16.1
+#       jupytext_version: 1.16.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -16,11 +16,12 @@
 #
 # # Data
 #
-# This section provides details about the three datasets simulated for this tutorial:
+# This section provides details about the various datasets simulated for this tutorial:
 #
-# - [Direct-current resistivity (DCR)](dc-data)
 # - [Magnetic total field (HeliTEM)](magnetic-data)
 # - [Airborne full-tensor gravity gradiometry (FTG)](ftg-data)
+# - [Direct-current resistivity (DCR)](dc-data)
+# - [Airborne Tipper (NSEM)](tipper-data)
 #
 #
 # ```{figure} ./images/project_area.png
@@ -30,24 +31,6 @@
 # ---
 # Data coverage for the ground gravity (black), airborne gravity gradiometry (red), airborne magnetics (green) and direct-current (blue) surveys. Outline of the ore shell (gray) is shown for reference.
 # ```
-#
-# <p style="page-break-after:always;"></p>
-
-# (dc-data)=
-#
-# ## Direct-current resistivity (DCR)
-#
-# A ground dipole-dipole survey was simulated over the main ore body. The survey was acquired with 40 m dipole length, data measured on seven receivers per transmitter dipole. Line separation was set to 100 m ([Figure 5](dc_topo)).
-#
-# ```{figure} ./images/DCR_data.png
-# ---
-# scale: 50%
-# name: dc_topo
-# ---
-# Pseudo-sections of apparent resisitvity and topography from the SRTM elevation model.
-# ```
-#
-# Note the large contrast in apparent resistivity between the western and eastern regions of the survey. This can be explained by a conductive tailing layer West of the deposit.
 #
 # <p style="page-break-after:always;"></p>
 
@@ -107,3 +90,43 @@
 # Vertical component of the gravity field (gz) measured over the zone of interest.
 # ```
 #
+
+# (dc-data)=
+#
+# ## Direct-current resistivity (DCR)
+#
+# A ground dipole-dipole survey was simulated over the main ore body. The survey was acquired with 40 m dipole length, data measured on seven receivers per transmitter dipole. Line separation was set to 100 m ([Figure 5](dc_topo)).
+#
+# ```{figure} ./images/DCR_data.png
+# ---
+# scale: 50%
+# name: dc_topo
+# ---
+# Pseudo-sections of apparent resisitvity and topography from the SRTM elevation model.
+# ```
+#
+# Note the large contrast in apparent resistivity between the western and eastern regions of the survey. This can be explained by a conductive tailing layer West of the deposit.
+#
+# <p style="page-break-after:always;"></p>
+
+# (tipper-data)=
+#
+# ## Airborne tipper (Natural Source EM)
+#
+# An airborne tipper survey was simulated over the main ore body. The survey was acquired along East-West lines, 400 m spacing, at a drape height of 60 m ([Figure 5](dc_topo)).
+# Tipper systems collect three orthogonal components of the ambiant magnetic field ($H_x,\;H_y,\;H_z$) in time. Similar to magnetotelluric surveys, the ratios of the components are used to circumvent the unknown source of the EM signal. The transfer functions, or tipper, are calculated by taking the ratio of the fields for two polarizations of the magnetic fields (EW and NS). The measurements are generally provided in the frequency-domain with both a real and an imaginary component.
+# For more background information about natural source methods, see [em.geosci](https://em.geosci.xyz/content/geophysical_surveys/ztem/index.html)
+#
+# The survey used in this tutorial mimics the commercial [ZTEM](https://geotech.ca/services/electromagnetic/ztem-z-axis-tipper-electromagnetic-system/) system that measures the magnetic field at 6 frequencies (30, 45, 90, 180, 360 and 720 Hz). The base station is located at the center of the survey area.
+#
+# ```{figure} ./images/tipper_data.png
+# ---
+# scale: 50%
+# name: tipper_topo
+# ---
+# Tipper data displayed by the 2D Profiler at all 6 frequencies for the 4 components ($T_{zx}$, $T_{zy}$, real and imaginary).
+# ```
+#
+# Note that the position of the known conductor correlate with large variations in the tipper data.
+#
+# <p style="page-break-after:always;"></p>
