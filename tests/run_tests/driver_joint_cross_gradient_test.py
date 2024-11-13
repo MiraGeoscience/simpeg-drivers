ac692c51b952c8e65975496d396874cd197d6dc6
dominiquef <dominiquef@mirageoscience.com>
2024-11-13 13:52:51 -0800

    Update test

--- tests/run_tests/driver_joint_cross_gradient_test.py
+++ tests/run_tests/driver_joint_cross_gradient_test.py
@@ -40,7 +40,7 @@ from simpeg_drivers.utils.utils import get_inversion_output
 # To test the full run and validate the inversion.
 # Move this file out of the test directory and run.
 
-target_run = {"data_norm": 53.296011, "phi_d": 10950, "phi_m": 0.09101}
+target_run = {"data_norm": 53.29601, "phi_d": 10590, "phi_m": 0.1336}
 
 
 def test_joint_cross_gradient_fwr_run(
