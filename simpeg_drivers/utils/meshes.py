bc34bcb2e7683f2828437b00f5ab884cc5aac161
benjamink <benjamink@mirageoscience.com>
2024-11-18 14:10:55 -0800

    First pass on auto_mesh

---
+++ simpeg_drivers/utils/meshes.py
@@ -0,0 +1,104 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#  All rights reserved.
+#
+#  This file is part of simpeg-drivers.
+#
+#  The software and information contained herein are proprietary to, and
+#  comprise valuable trade secrets of, Mira Geoscience, which
+#  intend to preserve as trade secrets such software and information.
+#  This software is furnished pursuant to a written license agreement and
+#  may be used, copied, transmitted, and stored only in accordance with
+#  the terms of such license and with the inclusion of the above copyright
+#  notice.  This software and information or any other copies thereof may
+#  not be provided or otherwise made available to any other person.
+#
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+import numpy as np
+from geoh5py.objects import ObjectBase, Octree
+from geoh5py.shared.utils import fetch_active_workspace
+from octree_creation_app.driver import OctreeDriver
+from octree_creation_app.params import OctreeParams
+
+from simpeg_drivers.utils.surveys import station_spacing
+
+
+def auto_pad(survey, factor=3) -> tuple[list[float], list[float]]:
+    """
+    Estimate horizontal padding as a fraction of the survey extent.
+
+    :param survey: Array of survey locations.
+    :param factor: Horizontal padding is the largest survey span (x or y)
+        divided by the factor value, ex: factor=2 will pad the survey in
+        all horizontal directions by half the largest survey extent.
+    :returns horizontal_padding: List of horizontal padding values.
+    :returns vertical_padding: List of vertical padding values
+    """
+
+    xmin = survey[:, 0].min()
+    xmax = survey[:, 0].max()
+    ymin = survey[:, 1].min()
+    ymax = survey[:, 1].max()
+    xrange = xmax - xmin
+    yrange = ymax - ymin
+    horizontal_padding = np.max([xrange, yrange]) / factor
+
+    return [horizontal_padding] * 4, [200.0] * 2
+
+
+def auto_mesh(
+    survey: ObjectBase,
+    topography: ObjectBase,
+    survey_refinement: list[int] | None = None,
+    topography_refinement: int = 2,
+    cell_size_factor: int = 2,
+) -> Octree:
+    """
+    Return a mesh optimized for the provided data extents and spacing.
+
+    :param survey: Survey object.
+    :param topography: Topography object.
+    :survey_refinement: List specifying how many cells at each octree level
+        to refine about the survey locations.
+    :topography_refinement: Number of cells to refine in the third octree
+        level below the topography.
+    :cell_size_factor: Sets the base cell size so that on average, there
+        will be N cells between each survey location, where N is the cell
+        size factor.
+    """
+
+    if survey_refinement is None:
+        survey_refinement = [4, 4, 4]
+
+    with fetch_active_workspace(survey.workspace, mode="r+") as workspace:
+        spacing = station_spacing(survey.locations) / cell_size_factor
+        base_cell_size = np.round(spacing)
+        horizontal_padding, vertical_padding = auto_pad(survey.locations)
+
+        params_dict = {
+            "geoh5": workspace,
+            "objects": survey,
+            "u_cell_size": base_cell_size,
+            "v_cell_size": base_cell_size,
+            "w_cell_size": base_cell_size,
+            "horizontal_padding": horizontal_padding,
+            "vertical_padding": vertical_padding,
+            "depth_core": 500,
+            "diagonal_balance": True,
+            "Refinement A object": survey,
+            "Refinement A levels": survey_refinement,
+            "Refinement A horizon": False,
+            "Refinement B object": topography,
+            "Refinement B levels": [0, 0, topography_refinement],
+            "Refinement B horizon": True,
+            "minimum_level": 8,
+        }
+        params = OctreeParams(**params_dict)
+        params.write_input_file(
+            name="automesh", path=workspace.h5file.parent, validate=False
+        )
+        driver = OctreeDriver(params)
+        mesh = driver.run()
+
+        return mesh
---
+++ tests/utils_meshes_test.py
@@ -0,0 +1,87 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#  All rights reserved.
+#
+#  This file is part of simpeg-drivers.
+#
+#  The software and information contained herein are proprietary to, and
+#  comprise valuable trade secrets of, Mira Geoscience, which
+#  intend to preserve as trade secrets such software and information.
+#  This software is furnished pursuant to a written license agreement and
+#  may be used, copied, transmitted, and stored only in accordance with
+#  the terms of such license and with the inclusion of the above copyright
+#  notice.  This software and information or any other copies thereof may
+#  not be provided or otherwise made available to any other person.
+#
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+import numpy as np
+from geoh5py import Workspace
+from geoh5py.objects import Points
+from perlin_noise import PerlinNoise
+from scipy.interpolate import LinearNDInterpolator
+
+from simpeg_drivers.utils.meshes import auto_mesh, auto_pad
+from tests.utils_surveys_test import create_test_survey
+
+
+def generate_random_topography(survey: np.ndarray, drape_survey: float | None = None):
+    """
+    Generate random topography over the survey area with optional drape.
+
+    :param survey: Array of survey locations.
+    :param drape_survey: If not none, the survey z values will be replaced with
+        an interpolation of the topography and shifted by the provided value.
+    """
+
+    xmin = survey[:, 0].min()
+    xmax = survey[:, 0].max()
+    ymin = survey[:, 1].min()
+    ymax = survey[:, 1].max()
+    xrange = xmax - xmin
+    yrange = ymax - ymin
+
+    topo_x = np.linspace(xmin - 0.5 * xrange, xmax + 0.5 * xrange, 100)
+    topo_y = np.linspace(ymin - 0.5 * yrange, ymax + 0.5 * yrange, 100)
+    topo_grid_x, topo_grid_y = np.meshgrid(topo_x, topo_y)
+
+    noise = PerlinNoise(octaves=5, seed=1)
+    xpix, ypix = topo_grid_x.shape[0], topo_grid_x.shape[1]
+    elevs = 200 * np.vstack(
+        [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
+    )
+    topo = np.c_[topo_grid_x.flatten(), topo_grid_y.flatten(), elevs.flatten()]
+
+    if drape_survey is not None:
+        interp = LinearNDInterpolator(topo[:, :2], topo[:, 2])
+        survey[:, 2] = interp(survey[:, :2]) + drape_survey
+
+    return survey, topo
+
+
+def test_auto_pad():
+    x = np.linspace(0, 1000, 101)
+    y = np.linspace(0, 500, 501)
+    x_grid, y_grid = np.meshgrid(x, y)
+    z = np.ones_like(x_grid)
+    survey = np.c_[x_grid.flatten(), y_grid.flatten(), z.flatten()]
+    horizontal_padding, vertical_padding = auto_pad(survey, factor=2)
+    assert horizontal_padding == [500, 500, 500, 500]
+    assert vertical_padding == [200, 200]
+    # TODO make sense of the padding and add an assertion
+
+
+def test_auto_mesh(tmp_path):
+    ws = Workspace(tmp_path / "test.geoh5")
+    locs = create_test_survey()
+    locs, topo = generate_random_topography(locs, drape_survey=0)
+
+    survey = Points.create(ws, name="survey", vertices=locs)
+    topo = Points.create(ws, name="topography", vertices=topo)
+
+    mesh = auto_mesh(survey, topo)
+
+    assert mesh.u_cell_size == 10
+    assert mesh.v_cell_size == 10
+    assert mesh.w_cell_size == 10
+    assert True
