20c2aa09db428a23a17840d1cdb89575e6b2af9e
benjamink <benjamink@mirageoscience.com>
2024-11-28 14:37:52 -0800

    re-organize sensitivity runtests

--- tests/run_tests/sensitivity_cutoff_test.py
+++ tests/run_tests/sensitivity_cutoff_test.py
@@ -33,13 +33,12 @@ from simpeg_drivers.potential_fields.gravity.driver import GravityDriver
 from simpeg_drivers.utils.testing import setup_inversion_workspace
 
 
-def test_gravity_fwr_run(
+def setup_inversion_results(
     tmp_path: Path,
     n_grid_points=2,
     refinement=(2,),
 ):
-    # Run the forward
-    geoh5, _, model, survey, topography = setup_inversion_workspace(
+    geoh5, mesh, model, survey, topography = setup_inversion_workspace(
         tmp_path,
         background=0.0,
         anomaly=0.75,
@@ -48,62 +47,43 @@ def test_gravity_fwr_run(
         refinement=refinement,
         flatten=False,
     )
+
+    # Run the inverse with save_sensitivities=True
+    gz = survey.add_data({"gz": {"values": np.random.randn(len(survey.vertices))}})
     params = GravityParams(
-        forward_only=True,
         geoh5=geoh5,
-        mesh=model.parent.uid,
+        mesh=mesh.uid,
         topography_object=topography.uid,
+        data_object=gz.parent.uid,
+        starting_model=1e-4,
+        reference_model=0.0,
+        s_norm=0.0,
+        gradient_type="components",
+        gz_channel_bool=True,
         z_from_topo=False,
-        data_object=survey.uid,
-        starting_model=model.uid,
+        gz_channel=gz.uid,
+        gz_uncertainty=2e-3,
+        lower_bound=0.0,
+        max_global_iterations=1,
+        initial_beta_ratio=1e-2,
+        prctile=100,
+        store_sensitivities="ram",
+        save_sensitivities=True,
     )
-    fwr_driver = GravityDriver(params)
-    fwr_driver.run()
-
-
-def test_gravity_run(
-    tmp_path: Path,
-    max_iterations=1,
-    pytest=True,
-):
-    workpath = tmp_path / "inversion_test.ui.geoh5"
-    if pytest:
-        workpath = tmp_path.parent / "test_gravity_fwr_run0" / "inversion_test.ui.geoh5"
-
-    with Workspace(workpath) as geoh5:
-        gz = geoh5.get_entity("Iteration_0_gz")[0]
-        mesh = geoh5.get_entity("mesh")[0]
-        topography = geoh5.get_entity("topography")[0]
-
-        # Run the inverse
-        params = GravityParams(
-            geoh5=geoh5,
-            mesh=mesh.uid,
-            topography_object=topography.uid,
-            data_object=gz.parent.uid,
-            starting_model=1e-4,
-            reference_model=0.0,
-            s_norm=0.0,
-            gradient_type="components",
-            gz_channel_bool=True,
-            z_from_topo=False,
-            gz_channel=gz.uid,
-            gz_uncertainty=2e-3,
-            lower_bound=0.0,
-            max_global_iterations=max_iterations,
-            initial_beta_ratio=1e-2,
-            prctile=100,
-            store_sensitivities="ram",
-            save_sensitivities=True,
-        )
-        params.write_input_file(path=tmp_path, name="Inv_run")
-
+    params.write_input_file(path=tmp_path, name="Inv_run")
     GravityDriver.start(str(tmp_path / "Inv_run.ui.json"))
 
-    with Workspace(workpath) as geoh5:
+
+def test_sensitivity_percent_cutoff_run(tmp_path):
+    setup_inversion_results(
+        tmp_path,
+        n_grid_points=2,
+        refinement=(2,),
+    )
+
+    with Workspace(tmp_path / "inversion_test.ui.geoh5") as geoh5:
         sensitivity = geoh5.get_entity("Iteration_1_sensitivities")[0]
         mesh = sensitivity.parent
-
         params = SensitivityCutoffParams(
             geoh5=geoh5,
             mesh=mesh,
@@ -115,11 +95,21 @@ def test_gravity_run(
         params.write_ui_json(path=tmp_path / "sensitivity_cutoff_percent")
 
     SensitivityCutoffDriver.start(str(tmp_path / "sensitivity_cutoff_percent.ui.json"))
+    with Workspace(tmp_path / "inversion_test.ui.geoh5") as geoh5:
+        mask = geoh5.get_entity("5 percent cutoff")[0]
+        assert mask.values.sum() == 1355
 
-    with Workspace(workpath) as geoh5:
-        # mask = geoh5.get_entity("5 percent cutoff")[0]
-        # assert mask.values.sum() == 1355
 
+def test_sensitivity_cutoff_percentile_run(tmp_path):
+    setup_inversion_results(
+        tmp_path,
+        n_grid_points=2,
+        refinement=(2,),
+    )
+
+    with Workspace(tmp_path / "inversion_test.ui.geoh5") as geoh5:
+        sensitivity = geoh5.get_entity("Iteration_1_sensitivities")[0]
+        mesh = sensitivity.parent
         params = SensitivityCutoffParams(
             geoh5=geoh5,
             mesh=mesh,
@@ -133,11 +123,21 @@ def test_gravity_run(
     SensitivityCutoffDriver.start(
         str(tmp_path / "sensitivity_cutoff_percentile.ui.json")
     )
-
-    with Workspace(workpath) as geoh5:
+    with Workspace(tmp_path / "inversion_test.ui.geoh5") as geoh5:
         mask = geoh5.get_entity("5 percentile cutoff")[0]
         assert mask.values.sum() == 22861
 
+
+def test_sensitivity_cutoff_log_percent_run(tmp_path):
+    setup_inversion_results(
+        tmp_path,
+        n_grid_points=2,
+        refinement=(2,),
+    )
+
+    with Workspace(tmp_path / "inversion_test.ui.geoh5") as geoh5:
+        sensitivity = geoh5.get_entity("Iteration_1_sensitivities")[0]
+        mesh = sensitivity.parent
         params = SensitivityCutoffParams(
             geoh5=geoh5,
             mesh=mesh,
@@ -151,7 +151,6 @@ def test_gravity_run(
     SensitivityCutoffDriver.start(
         str(tmp_path / "sensitivity_cutoff_log_percent.ui.json")
     )
-
-    with Workspace(workpath) as geoh5:
+    with Workspace(tmp_path / "inversion_test.ui.geoh5") as geoh5:
         mask = geoh5.get_entity("5 percent log cutoff")[0]
         assert mask.values.sum() == 23144
