0d81634e17e6f35c6848283e323830147d189f5f
Sébastien Hensgen <sebastienh@mirageoscience.com>
2025-01-12 10:05:30 -0500

    [GEOPY-1933] fix typo in copyright statements

--- tests/constants_test.py
+++ tests/constants_test.py
@@ -1,7 +1,7 @@
 # '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 #  Copyright (c) 2024-2025 Mira Geoscience Ltd.                                     '
 #                                                                                   '
-#  This file is part simpeg-drivers package.                                        '
+#  This file is part of simpeg-drivers package.                                     '
 #                                                                                   '
 #  simpeg-drivers is distributed under the terms and conditions of the MIT License  '
 #  (see LICENSE file at the root of this source code package).                      '
