fa18a3562f04770abe673967a98877683298a97b
dominiquef <dominiquef@mirageoscience.com>
2025-02-06 16:44:46 -0800

    add conf.py for RTD

---
+++ docs/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from importlib.metadata import version
+from datetime import datetime
+from packaging.version import Version
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "simpeg-drivers"
+
+# The full version, including alpha/beta/rc tags.
+release = version("simpeg-drivers")
+# The short X.Y.Z version.
+version = Version(release).base_version
+
+project_copyright = "%Y, Mira Geoscience Ltd"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+autodoc_mock_imports = [
+    "geoapps_utils",
+    "geoh5py",
+    "numpy",
+]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+todo_include_todos = True
+
+# -- Options for auto-doc ----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#module-sphinx.ext.autodoc
+
+autodoc_typehints = "signature"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_theme_options = {
+    "description": f"version {release}",
+}
+
+# Enable numref
+numfig = True
+
+
+def get_copyright_notice():
+    return f"Copyright {datetime.now().strftime(project_copyright)}"
+
+
+rst_epilog = f"""
+.. |copyright_notice| replace:: {get_copyright_notice()}.
+"""
