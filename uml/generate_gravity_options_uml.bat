895a4d3d33c91a8cebcd2c83b6bf689ba1a94c23
benk-mira <81254271+benk-mira@users.noreply.github.com>
2025-02-19 13:49:41 -0800

    Update uml/generate_gravity_options_uml.bat
    Co-authored-by: Sébastien Hensgen <sebastienh@mirageoscience.com>

--- uml/generate_gravity_options_uml.bat
+++ uml/generate_gravity_options_uml.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-REM Set the relative path variable
-set "param_path=..\simpeg_drivers\potential_fields\gravity\params.py"
+set project_dir=%~dp0..
+set "param_path=%project_dir%\simpeg_drivers\potential_fields\gravity\params.py"
 
 call "pyreverse --output puml --all-ancestors --project gravity_params %param_path%"
 
