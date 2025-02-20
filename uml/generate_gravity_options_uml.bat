e70c662905d4132a1053dfea9816f123a3530eed
benjamink <benjamink@mirageoscience.com>
2025-02-20 08:35:08 -0800

    remove quotes from pyreverse .bat

--- uml/generate_gravity_options_uml.bat
+++ uml/generate_gravity_options_uml.bat
@@ -3,8 +3,7 @@ setlocal
 
 set project_dir=%~dp0..
 set "param_path=%project_dir%\simpeg_drivers\potential_fields\gravity\params.py"
-
-call "pyreverse --output puml --all-ancestors --project gravity_params %param_path%"
+call pyreverse --output puml --all-ancestors --project gravity_params %param_path%
 
 pause
 
