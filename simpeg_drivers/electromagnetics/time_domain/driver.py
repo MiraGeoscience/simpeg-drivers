c7a49cc4c20dece7025f237082d86198ab6acf61
dominiquef <dominiquef@mirageoscience.com>
2025-03-13 13:03:21 -0700

    Reduce code duplication

--- simpeg_drivers/electromagnetics/time_domain/driver.py
+++ simpeg_drivers/electromagnetics/time_domain/driver.py
@@ -25,6 +25,62 @@ from .params import (
 )
 
 
+def tile_large_group_transmitters(
+    survey: LargeLoopGroundTEMReceivers, n_tiles: int
+) -> list[np.ndarray]:
+    """
+    Tile the data based on the transmitters center locations.
+
+    :param survey: LargeLoopGroundTEMReceivers object.
+    :param n_tiles: Number of tiles.
+
+    :return: List of numpy arrays containing the indices of the receivers in each tile.
+    """
+    if not isinstance(survey, LargeLoopGroundTEMReceivers):
+        raise TypeError("Data object must be of type LargeLoopGroundTEMReceivers")
+
+    tx_ids = survey.transmitters.tx_id_property.values
+    unique_tile_ids = np.unique(tx_ids)
+    n_groups = np.min([len(unique_tile_ids), n_tiles])
+    locations = []
+    for uid in unique_tile_ids:
+        locations.append(
+            np.mean(
+                survey.transmitters.vertices[tx_ids == uid],
+                axis=0,
+            )
+        )
+
+    # Tile transmitters spatially by loop center
+    tx_tiles = tile_locations(
+        np.vstack(locations),
+        n_groups,
+        method="kmeans",
+    )
+    receivers_tx_ids = survey.tx_id_property.values
+    tiles = []
+    for _t_id, group in enumerate(tx_tiles):
+        sub_group = []
+        for value in group:
+            receiver_ind = receivers_tx_ids == unique_tile_ids[value]
+            sub_group.append(np.where(receiver_ind)[0])
+
+        tiles.append(np.hstack(sub_group))
+
+    # If number of tiles remaining, brake up receivers spatially per transmitter
+    while len(tiles) < n_tiles:
+        largest_group = np.argmax([len(tile) for tile in tiles])
+        tile = tiles.pop(largest_group)
+        new_tiles = tile_locations(
+            survey.vertices[tile],
+            2,
+            method="kmeans",
+        )
+        tiles += [tile[new_tiles[0]], tile[new_tiles[1]]]
+
+    return tiles
+
+
 class TDEMForwardDriver(InversionDriver):
     """Time Domain Electromagnetic forward driver."""
 
@@ -42,46 +98,10 @@ class TDEMForwardDriver(InversionDriver):
         if not isinstance(self.params.data_object, LargeLoopGroundTEMReceivers):
             return super().get_tiles()
 
-        tx_ids = self.params.data_object.transmitters.tx_id_property.values
-        unique_tile_ids = np.unique(tx_ids)
-        n_groups = np.min([len(unique_tile_ids), self.params.tile_spatial])
-        locations = []
-        for uid in unique_tile_ids:
-            locations.append(
-                np.mean(
-                    self.params.data_object.transmitters.vertices[tx_ids == uid],
-                    axis=0,
-                )
-            )
-
-        # Tile transmitters spatially by loop center
-        tx_tiles = tile_locations(
-            np.vstack(locations),
-            n_groups,
-            method="kmeans",
+        return tile_large_group_transmitters(
+            self.params.data_object,
+            self.params.tile_spatial,
         )
-        receivers_tx_ids = self.params.data_object.tx_id_property.values
-        tiles = []
-        for _t_id, group in enumerate(tx_tiles):
-            sub_group = []
-            for value in group:
-                receiver_ind = receivers_tx_ids == unique_tile_ids[value]
-                sub_group.append(np.where(receiver_ind)[0])
-
-            tiles.append(np.hstack(sub_group))
-
-        # If number of tiles remaining, brake up receivers spatially per transmitter
-        while len(tiles) < self.params.tile_spatial:
-            largest_group = np.argmax([len(tile) for tile in tiles])
-            tile = tiles.pop(largest_group)
-            new_tiles = tile_locations(
-                self.params.data_object.vertices[tile],
-                2,
-                method="kmeans",
-            )
-            tiles += [tile[new_tiles[0]], tile[new_tiles[1]]]
-
-        return tiles
 
 
 class TDEMInversionDriver(InversionDriver):
@@ -101,43 +121,7 @@ class TDEMInversionDriver(InversionDriver):
         if not isinstance(self.params.data_object, LargeLoopGroundTEMReceivers):
             return super().get_tiles()
 
-        tx_ids = self.params.data_object.transmitters.tx_id_property.values
-        unique_tile_ids = np.unique(tx_ids)
-        n_groups = np.min([len(unique_tile_ids), self.params.tile_spatial])
-        locations = []
-        for uid in unique_tile_ids:
-            locations.append(
-                np.mean(
-                    self.params.data_object.transmitters.vertices[tx_ids == uid],
-                    axis=0,
-                )
-            )
-
-        # Tile transmitters spatially by loop center
-        tx_tiles = tile_locations(
-            np.vstack(locations),
-            n_groups,
-            method="kmeans",
+        return tile_large_group_transmitters(
+            self.params.data_object,
+            self.params.tile_spatial,
         )
-        receivers_tx_ids = self.params.data_object.tx_id_property.values
-        tiles = []
-        for _t_id, group in enumerate(tx_tiles):
-            sub_group = []
-            for value in group:
-                receiver_ind = receivers_tx_ids == unique_tile_ids[value]
-                sub_group.append(np.where(receiver_ind)[0])
-
-            tiles.append(np.hstack(sub_group))
-
-        # If number of tiles remaining, brake up receivers spatially per transmitter
-        while len(tiles) < self.params.tile_spatial:
-            largest_group = np.argmax([len(tile) for tile in tiles])
-            tile = tiles.pop(largest_group)
-            new_tiles = tile_locations(
-                self.params.data_object.vertices[tile],
-                2,
-                method="kmeans",
-            )
-            tiles += [tile[new_tiles[0]], tile[new_tiles[1]]]
-
-        return tiles
