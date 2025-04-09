65819fedb549016119d97d27af3c5b5a96306cf0
benjamink <benjamink@mirageoscience.com>
2025-04-09 11:56:06 -0700

    fix warnings on data_test and drivers_test

--- tests/driver_test.py
+++ tests/driver_test.py
@@ -46,7 +46,6 @@ def test_smallness_terms(tmp_path: Path):
             alpha_s=1.0,
             s_norm=0.0,
             gradient_type="components",
-            gz_channel_bool=True,
             gz_channel=gz,
             gz_uncertainty=2e-3,
             lower_bound=0.0,
