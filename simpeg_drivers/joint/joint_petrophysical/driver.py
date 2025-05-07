d06fac485e55ebe48e7883cb5e3189f9a0fa9f3b
dominiquef <dominiquef@mirageoscience.com>
2025-05-07 14:28:06 -0400

    Add main files for PGI

---
+++ simpeg_drivers/joint/joint_petrophysical/driver.py
@@ -0,0 +1,52 @@
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2025 Mira Geoscience Ltd.                                          '
+#                                                                                   '
+#  This file is part of simpeg-drivers package.                                     '
+#                                                                                   '
+#  simpeg-drivers is distributed under the terms and conditions of the MIT License  '
+#  (see LICENSE file at the root of this source code package).                      '
+#                                                                                   '
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+from __future__ import annotations
+
+from itertools import combinations
+
+import numpy as np
+from geoh5py.groups.property_group_type import GroupTypeEnum
+from geoh5py.shared.utils import fetch_active_workspace
+from simpeg import directives, maps
+from simpeg.objective_function import ComboObjectiveFunction
+from simpeg.regularization import PGI
+
+from simpeg_drivers.components.factories import (
+    DirectivesFactory,
+    SaveModelGeoh5Factory,
+)
+from simpeg_drivers.joint.driver import BaseJointDriver
+
+from .options import JointPetrophysicalOptions
+
+
+class JointPetrophysicalDriver(BaseJointDriver):
+    _options_class = JointPetrophysicalOptions
+    _validations = None
+
+    def __init__(self, params: JointPetrophysicalOptions):
+        self._wires = None
+        self._directives = None
+
+        super().__init__(params)
+
+        with fetch_active_workspace(self.workspace, mode="r+"):
+            self.initialize()
+
+    def get_regularization(self):
+        """
+        Create a flat ComboObjectiveFunction from all drivers provided and
+        add cross-gradient regularization for all combinations of model parameters.
+        """
+        regularizations = super().get_regularization()
+        reg_list, multipliers = self._overload_regularization(regularizations)
+
+        return ComboObjectiveFunction(objfcts=reg_list, multipliers=multipliers)
---
+++ simpeg_drivers/joint/joint_petrophysical/options.py
@@ -0,0 +1,50 @@
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2025 Mira Geoscience Ltd.                                          '
+#                                                                                   '
+#  This file is part of simpeg-drivers package.                                     '
+#                                                                                   '
+#  simpeg-drivers is distributed under the terms and conditions of the MIT License  '
+#  (see LICENSE file at the root of this source code package).                      '
+#                                                                                   '
+# '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import ClassVar
+
+from geoh5py.data import ReferencedData
+from geoh5py.groups import SimPEGGroup
+from geoh5py.objects import Octree
+
+from simpeg_drivers import assets_path
+from simpeg_drivers.joint.options import BaseJointOptions
+
+
+class JointPetrophysicalOptions(BaseJointOptions):
+    """
+    Joint Petrophysically Guided Inversion (PGI) driver.
+
+    :param mesh: The global mesh entity containing the reference geology.
+    :param geo_model: The reference geology data.
+    """
+
+    name: ClassVar[str] = "Petrophysically Guided Inversion (PGI)"
+    default_ui_json: ClassVar[Path] = (
+        assets_path() / "uijson/joint_petrophysical_inversion.ui.json"
+    )
+
+    title: str = "Joint Petrophysically Guided Inversion (PGI)"
+    physical_property: list[str] = [""]
+
+    inversion_type: str = "joint petrophysical"
+
+    mesh: Octree
+    geo_model: ReferencedData
+    group_a: SimPEGGroup
+    group_a_multiplier: float | None = 1.0
+    group_b: SimPEGGroup | None = None
+    group_b_multiplier: float | None = None
+    group_c: SimPEGGroup | None = None
+    group_c_multiplier: float | None = None
