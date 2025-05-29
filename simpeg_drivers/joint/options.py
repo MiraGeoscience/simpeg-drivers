563d1a481e2e5f045e96ec9066a3c9db9b79efda
dominiquef <dominiquef@mirageoscience.com>
2025-05-29 14:00:28 -0700

    Allow None for phys prop on joint

--- simpeg_drivers/joint/options.py
+++ simpeg_drivers/joint/options.py
@@ -44,7 +44,7 @@ class BaseJointOptions(BaseData):
     run_command: str = "simpeg_drivers.driver"
     conda_environment: str = "simpeg-drivers"
     forward_only: bool = False
-    physical_property: str = ""
+    physical_property: str | None = None
 
     group_a: SimPEGGroup
     group_a_multiplier: float = 1.0
