f5be75bc8173d7e88e0361019a1207c14d6507e5
dominiquef <dominiquef@mirageoscience.com>
2025-06-02 08:18:04 -0700

    Create base class for TDEM options. Fix imports

--- simpeg_drivers/electromagnetics/frequency_domain/__init__.py
+++ simpeg_drivers/electromagnetics/frequency_domain/__init__.py
@@ -7,12 +7,3 @@
 #  (see LICENSE file at the root of this source code package).                      '
 #                                                                                   '
 # '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-
-from .options import (
-    FDEMForwardOptions,
-    FDEMInversionOptions,
-)
-
-# pylint: disable=unused-import
-# flake8: noqa
