14611190f5569019296295a451bfbd08d75f889a
saicheran <saicheranb@mirageoscience.com>
2025-08-27 20:26:51 -0400

    [DEVOPS-545] Update versioning strategy and implement dynamic versioning support

--- simpeg_drivers/__init__.py
+++ simpeg_drivers/__init__.py
@@ -11,12 +11,18 @@
 
 from __future__ import annotations
 
-
-__version__ = "0.4.0a1"
-
-
 import logging
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
+
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:  # pragma: no cover
+    from datetime import datetime
+
+    __date_str = datetime.today().strftime("%Y%m%d")
+    __version__ = "0.0.0.dev0+" + __date_str
 
 
 logging.basicConfig(level=logging.INFO)
