c88cc91aa22e71fcf46f1f06318a22d3ddfde75c
dominiquef <dominiquef@mirageoscience.com>
2025-11-21 13:54:56 -0800

    Switch to index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.todo",
 ]
-master_doc = 'index.md'
+master_doc = 'index'
 templates_path = ["_templates"]
 exclude_patterns = []
 todo_include_todos = True
