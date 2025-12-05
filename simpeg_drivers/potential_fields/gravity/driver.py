392a8f9a2cd902d1b69e9c233717657a90725228
benjamink <benjamink@mirageoscience.com>
2025-12-05 08:24:39 -0800

    get full traceback?

--- simpeg_drivers/potential_fields/gravity/driver.py
+++ simpeg_drivers/potential_fields/gravity/driver.py
@@ -17,6 +17,18 @@ from simpeg_drivers.potential_fields.gravity.options import (
     GravityInversionOptions,
 )
 
+import traceback
+import warnings
+import sys
+
+def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+
+    log = file if hasattr(file,'write') else sys.stderr
+    traceback.print_stack(file=log)
+    log.write(warnings.formatwarning(message, category, filename, lineno, line))
+
+warnings.showwarning = warn_with_traceback
+
 
 class GravityForwardDriver(InversionDriver):
     """Gravity forward driver."""
