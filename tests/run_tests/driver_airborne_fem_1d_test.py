faf805d6f2680f83c8e64a68d3dd722abd254ba3
domfournier <dominiquef@mirageoscience.com>
2026-03-23 15:31:14 -0700

    Change target on fem1d after removing double Directives in options

--- tests/run_tests/driver_airborne_fem_1d_test.py
+++ tests/run_tests/driver_airborne_fem_1d_test.py
@@ -38,7 +38,7 @@ from tests.utils.targets import check_target, get_inversion_output, get_workspac
 # To test the full run and validate the inversion.
 # Move this file out of the test directory and run.
 
-target_run = {"data_norm": 804.9849282354428, "phi_d": 58200, "phi_m": 118}
+target_run = {"data_norm": 804.9849282354428, "phi_d": 64500, "phi_m": 717}
 
 
 def test_fem_fwr_1d_run(
