46f55d00bb78c2cbb003893b8348ae53f97d5a04
domfournier <dominiquef@mirageoscience.com>
2026-04-07 08:36:23 -0700

    Update simpeg_drivers/plate_simulation/sweep/driver.py
    Co-authored-by: benk-mira <81254271+benk-mira@users.noreply.github.com>

--- simpeg_drivers/plate_simulation/sweep/driver.py
+++ simpeg_drivers/plate_simulation/sweep/driver.py
@@ -197,7 +197,7 @@ class PlateSweepDriver(Driver):
         cls, json_path: Path, n_workers: int | None = None, n_threads: int | None = None
     ):
         """
-        Sets Dask config settings.
+        Runs plate sweep application with Dask optimization
 
         :param json_path: Path to input file (.ui.json) for the application.
         :param n_workers: Number of workers to use.
