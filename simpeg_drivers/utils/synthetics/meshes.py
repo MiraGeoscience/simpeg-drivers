b661e96ea30c27e74650a2e77614dd427588fc1b
domfournier <dominiquef@mirageoscience.com>
2026-04-10 11:54:42 -0700

    Remove unused function

--- simpeg_drivers/utils/synthetics/meshes.py
+++ simpeg_drivers/utils/synthetics/meshes.py
@@ -59,42 +59,6 @@ def get_mesh(
     )
 
 
-def get_base_octree(
-    survey: Points,
-    topography: Surface,
-    cell_size: tuple[float, float, float],
-    refinement: tuple,
-    padding: float,
-) -> TreeMesh:
-    """
-    Generate a survey centered TreeMesh object with topography refinement.
-
-    :param survey: Survey object with vertices that define the core of the
-        tensor mesh.
-    :param topography: Surface used to refine the topography.
-    :param cell_size: Tuple defining the cell size in all directions.
-    :param refinement: Tuple containing the number of cells to refine at each
-        level around the topography.
-    :param padding: Distance to pad the mesh in all directions.
-
-    :return mesh: The discretize TreeMesh object for computations.
-    """
-    padding_distance = np.ones((3, 2)) * padding
-    mesh = mesh_builder_xyz(
-        survey.vertices - np.r_[cell_size] / 2.0,
-        cell_size,
-        depth_core=100.0,
-        padding_distance=padding_distance,
-        mesh_type="TREE",
-        tree_diagonal_balance=False,
-    )
-    mesh = OctreeDriver.refine_tree_from_triangulation(
-        mesh, topography, levels=refinement, finalize=False
-    )
-
-    return mesh
-
-
 def get_octree_mesh(
     opts: MeshOptions,
     survey: Points,
