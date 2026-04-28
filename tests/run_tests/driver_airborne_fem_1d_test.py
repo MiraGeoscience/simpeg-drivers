15470c97b40d07905c90065bc26e23142f7a0344
domfournier <dominiquef@mirageoscience.com>
2026-04-28 15:45:41 -0700

    Re-adjust target

--- tests/run_tests/driver_airborne_fem_1d_test.py
+++ tests/run_tests/driver_airborne_fem_1d_test.py
@@ -38,7 +38,7 @@ from tests.utils.targets import check_target, get_inversion_output, get_workspac
 # To test the full run and validate the inversion.
 # Move this file out of the test directory and run.
 
-target_run = {"data_norm": 380.3510229653626, "phi_d": 33100, "phi_m": 209}
+target_run = {"data_norm": 381.0495293422212, "phi_d": 33400, "phi_m": 198}
 
 
 def test_fem_fwr_1d_run(
