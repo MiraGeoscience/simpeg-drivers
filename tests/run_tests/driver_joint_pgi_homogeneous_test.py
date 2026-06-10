5477624ab04267fa2ecd9c7e7d4ac26712f4e329
domfournier <dominiquef@mirageoscience.com>
2026-06-10 14:39:32 -0700

    Revert change to test

--- tests/run_tests/driver_joint_pgi_homogeneous_test.py
+++ tests/run_tests/driver_joint_pgi_homogeneous_test.py
@@ -243,9 +243,7 @@ def test_homogeneous_run(
                     tmi_channel=data,
                     tmi_uncertainty=5e0,
                 )
-                driver = MagneticInversionDriver(params)
-                driver.out_group.options["reference_model"]["enabled"] = False
-                drivers.append(driver)
+                drivers.append(MagneticInversionDriver(params))
 
             if len(drivers) == 1:
                 # Test if single group is valid
